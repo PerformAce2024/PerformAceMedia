3a727cbdc7947b98993f86bc5480e5bd7de8d2a2
Prakhar <prakharb56@gmail.com>
2025-04-23 12:09:04 +0530

    updated nginx.conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,10 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    headers: {
-      "Content-Security-Policy":
-        "default-src 'self'; media-src 'self' https://pacreatives.s3.ap-south-1.amazonaws.com https://*.amazonaws.com; connect-src 'self' https://pacreatives.s3.ap-south-1.amazonaws.com https://*.amazonaws.com; img-src 'self' data: https://*.amazonaws.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline';",
-    },
-  },
 });
